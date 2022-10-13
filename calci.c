2037b80d70c547357ab1f8b0f3a9bd8352a60215
“[Adithya <[paiadithya26@gmail.com]>
2022-10-13 19:27:27 +0530

    Inital commoit

---
+++ calci.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+
+int main()
+{
+    int a,b;
+    char c;
+    printf("Enter the first number: ");
+    scanf("%d",&a);
+    printf("Enter the second number: ");
+    scanf("%d",&b);
+    printf("Enter the operator: ");
+    scanf(" %c",&c);
+    switch(c)
+    {
+        case '+':
+            printf("The sum is %d",a+b);
+            break;
+        case '-':
+            printf("The difference is %d",a-b);
+            break;
+        case '*':
+            printf("The product is %d",a*b);
+            break;
+        case '/':
+            printf("The quotient is %d",a/b);
+            break;
+        default:
+            printf("Invalid operator");
+    }
+    return 0;
+}
+
