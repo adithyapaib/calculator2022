f257a690a5de1489dde497d73bfaa2de837f6346
Deepak Nayak <107839233+github2004dn@users.noreply.github.com>
2023-02-13 23:36:51 +0530

    added 2 new files

---
+++ learninghr.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include<iomanip>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    
+    int a;
+    float b;
+    char c;
+    double d;
+    long l;
+    cin>>a>>l>>c>>b>>d;
+    cout<<a<<"\n";
+    cout<<l<<"\n";
+    cout<<c<<"\n";
+    cout<<fixed<<setprecision(2)<<b<<"\n";
+    cout<<d;
+    return 0;
+}
